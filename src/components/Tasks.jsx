f98bf9385fdb45abfad55bbf07545527d4813381
Isaac Gums Bodenmuller <isaac.gums@havan.com.br>
2026-09-10 14:32:31 -0300

    feat: add initial Task and Button component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,13 +1,28 @@
-import { Header } from "./Header"
+import Button from "./Button"
+import AddIcon from "../assets/icons/add.svg?react"
+import TrashIcon from "../assets/icons/trash.svg?react"
 
 export function Tasks() {
   return (
-    <div>
-      <Header>
-        <h1>Add a task</h1>
-      </Header>
-
-      <input type="text" />
+    <div className="w-full px-8 py-16">
+      <div className="flex w-full justify-between">
+        <div>
+          <span className="text-xs font-semibold text-[#00adb5]">
+            Minhas Tarefas
+          </span>
+          <h2 className="text-xl font-semibold">Minhas Tarefas</h2>
+        </div>
+        <div className="flex items-center gap-3">
+          <Button variant="ghost">
+            Limpar tarefa
+            <TrashIcon />
+          </Button>
+          <Button>
+            Nova tarefa
+            <AddIcon />
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
