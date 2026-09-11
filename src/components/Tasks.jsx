80365738a172839ccd260007e282de87e9bdc55c
Isaac Gums Bodenmuller <isaac.gums@havan.com.br>
2026-09-11 09:37:39 -0300

    feat: update spacing between sidebar and tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -34,7 +34,7 @@ export function Tasks() {
   }
 
   return (
-    <div className="w-full px-8 py-16">
+    <div className="w-full space-y-6 px-8 py-16">
       <div className="flex w-full justify-between">
         <div>
           <span className="text-xs font-semibold text-[#00adb5]">
