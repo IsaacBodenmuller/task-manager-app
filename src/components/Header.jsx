f1adadb883b640b7b4361709a1cadd8da1aa0e33
Isaac Gums Bodenmuller <isaac.gums@havan.com.br>
2026-09-14 10:27:57 -0300

    chore: add prop-types to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,9 @@
+import PropTypes from "prop-types"
+
 export function Header(props) {
   return <header className="mt-9 bg-red-500">{props.children}</header>
+}
+
+Header.propTypes = {
+  children: PropTypes.node.isRequired,
 }
