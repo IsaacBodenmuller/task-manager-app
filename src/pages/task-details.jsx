ea336c0b1a80568a569cdd85f3fd89436d2cff54
Isaac Gums Bodenmuller <isaac.gums@havan.com.br>
2026-09-16 17:25:49 -0300

    feat: add delete and update task-detail page

--- src/pages/task-details.jsx
+++ src/pages/task-details.jsx
@@ -1,7 +1,13 @@
-import { useEffect, useState } from "react"
-import { useNavigate, useParams } from "react-router-dom"
+import { useEffect, useRef, useState } from "react"
+import { Link, useNavigate, useParams } from "react-router-dom"
+import { toast } from "sonner"
 
-import { ArrowLeftIcon, ChevronRightIcon, TrashIcon } from "../assets/icons"
+import {
+  ArrowLeftIcon,
+  ChevronRightIcon,
+  LoaderIcon,
+  TrashIcon,
+} from "../assets/icons"
 import Button from "../components/Button"
 import Input from "../components/Input"
 import Sidebar from "../components/Sidebar"
@@ -10,11 +16,75 @@ import TimeSelect from "../components/TimeSelect"
 const TaskDetailsPage = () => {
   const { taskId } = useParams()
   const [task, setTask] = useState()
+  const [errors, setErrors] = useState([])
+  const [saveIsLoading, setSaveIsLoading] = useState(false)
+
   const navigate = useNavigate()
+
+  const titleRef = useRef()
+  const timeRef = useRef()
+  const descriptionRef = useRef()
 
   const handleBackClick = () => {
     navigate(-1)
   }
+
+  const handleDeleteTask = async (taskId) => {
+    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: "DELETE",
+    })
+    if (!response.ok) {
+      return toast.error("Erro ao deletar tarefa. Por favor tente novamente.")
+    }
+    handleBackClick()
+  }
+
+  const handleUpdateTask = async (taskId) => {
+    setSaveIsLoading(true)
+    const newErrors = []
+    const title = titleRef.current.value
+    const time = timeRef.current.value
+    const description = descriptionRef.current.value
+
+    if (!title.trim()) {
+      newErrors.push({ inputName: "title", message: "O título é obrigatório." })
+    }
+    if (!time.trim()) {
+      newErrors.push({ inputName: "time", message: "O horário é obrigatório." })
+    }
+    if (!description.trim()) {
+      newErrors.push({
+        inputName: "description",
+        message: "A descrição é obrigatória.",
+      })
+    }
+
+    setErrors(newErrors)
+    if (newErrors.length > 0) {
+      return setSaveIsLoading(false)
+    }
+    console.log("a")
+    const taskUpdated = { title, time, description }
+    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: "PATCH",
+      body: JSON.stringify(taskUpdated),
+    })
+    if (!response.ok) {
+      setSaveIsLoading(false)
+      return toast.error("Erro ao atualizar tarefa. Por favor tente novamente.")
+    }
+
+    const newTask = await response.json()
+    setTask(newTask)
+    setSaveIsLoading(false)
+    // handleBackClick()
+  }
+
+  const titleError = errors.find((error) => error.inputName === "title")
+  const timeError = errors.find((error) => error.inputName === "time")
+  const descriptionError = errors.find(
+    (error) => error.inputName === "description"
+  )
 
   useEffect(() => {
     const fetchTask = async () => {
@@ -27,6 +97,7 @@ const TaskDetailsPage = () => {
     }
     fetchTask()
   }, [taskId])
+
   return (
     <div className="flex">
       <Sidebar />
@@ -40,12 +111,9 @@ const TaskDetailsPage = () => {
               <ArrowLeftIcon />
             </button>
             <div className="flex items-center gap-1 text-xs">
-              <span
-                onClick={handleBackClick}
-                className="cursor-pointer text-brand-text-gray"
-              >
+              <Link className="cursor-pointer text-brand-text-gray" to="/">
                 Minhas Tarefas
-              </span>
+              </Link>
               <ChevronRightIcon className="text-brand-text-gray" />
               <span className="font-semibold text-brand-primary">
                 {task?.title}
@@ -54,8 +122,12 @@ const TaskDetailsPage = () => {
             <h1 className="mt-2 text-xl font-semibold">{task?.title}</h1>
           </div>
 
-          <div>
-            <Button className="h-fit self-end" color="danger">
+          <div className="self-end">
+            <Button
+              className="h-fit self-end"
+              color="danger"
+              onClick={() => handleDeleteTask(taskId)}
+            >
               <TrashIcon />
               Deletar tarefa
             </Button>
@@ -64,16 +136,28 @@ const TaskDetailsPage = () => {
 
         <div className="space-y-6 rounded-xl bg-brand-white p-6">
           <div>
-            <Input id="title" label="Título" value={task?.title} />
+            <Input
+              id="title"
+              label="Título"
+              ref={titleRef}
+              defaultValue={task?.title}
+              errorMessage={titleError?.message}
+            />
           </div>
           <div>
-            <TimeSelect value={task?.time} />
+            <TimeSelect
+              ref={timeRef}
+              defaultValue={task?.time}
+              errorMessage={timeError?.message}
+            />
           </div>
           <div>
             <Input
               id="description"
               label="Descrição"
-              value={task?.description}
+              ref={descriptionRef}
+              defaultValue={task?.description}
+              errorMessage={descriptionError?.message}
             />
           </div>
         </div>
@@ -82,7 +166,12 @@ const TaskDetailsPage = () => {
           <Button color="secondary" size="large">
             Cancelar
           </Button>
-          <Button color="primary" size="large">
+          <Button
+            color="primary"
+            size="large"
+            onClick={() => handleUpdateTask(taskId)}
+          >
+            {saveIsLoading && <LoaderIcon />}
             Salvar
           </Button>
         </div>
